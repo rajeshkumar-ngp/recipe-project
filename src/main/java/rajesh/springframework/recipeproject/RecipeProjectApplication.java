2d2d9127e9b3ab2b554d1a118391e90b170d741d
rajeshkumar-ngp <rajesh_kumar_ngp@yahoo.co.in>
2021-09-30 17:17:16 +0530

    Recipe note, Ingred relational Mapping

--- src/main/java/rajesh/springframework/recipeproject/RecipeProjectApplication.java
+++ src/main/java/rajesh/springframework/recipeproject/RecipeProjectApplication.java
@@ -8,6 +8,7 @@ public class RecipeProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeProjectApplication.class, args);
+		System.out.println("Some Text 123");
 	}
 
 }
---
+++ src/main/java/rajesh/springframework/recipeproject/domain/UnitOfMeasure.java
@@ -0,0 +1,29 @@
+package rajesh.springframework.recipeproject.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
